bcc9ad2bcd335c9a39bca343ff3ebf11920a2dfa
冰块 <bingkuai.ywj@alibaba-inc.com>
2021-06-25 14:04:14 +0800

    feat: 更简单的搜索形式和支持解构赋值

--- packages/esbuild-import-plugin/test/esbuild.config.js
+++ packages/esbuild-import-plugin/test/esbuild.config.js
@@ -1,8 +1,21 @@
 const esbuild = require('esbuild')
+const importPlguin = require('../index')
 
 esbuild.build({
-    entryPoints: ['./test/index'],
-    bundle: true,
-    minify: true,
-    outdir: 'dist',
+  entryPoints: ['./test/index'],
+  bundle: false,
+  minify: false,
+  outdir: 'dist',
+  format: 'cjs',
+  plugins: [
+    importPlguin({
+      options: [
+        {
+          libraryName: 'antd',
+          libraryDirectory: 'lib',
+          style: true,
+        },
+      ],
+    }),
+  ],
 })
--- packages/esbuild-import-plugin/test/index.jsx
+++ packages/esbuild-import-plugin/test/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Button} from 'antd'
+import Antd, { TimePicker } from 'antd'
+import MyButton from 'antd/lib/button'
+// const MyButton = Antd.Button
+const { Table, Select: MySelect } = Antd
+const { Input } = Antd
 
 const App = () => {
-    return <Button>foo</Button>
+  return (
+    <>
+      <MyButton />
+      <TimePicker />
+      <Input />
+      <Table />
+      <MySelect />
+      <Antd.Button>click me!!</Antd.Button>
+    </>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
