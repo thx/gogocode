1d2022e865f17f1cd7ee7343a3eb788bd401ed54
wangmeng <wangmeng01@alibaba-inc.com>
2021-06-25 15:16:23 +0800

    feat: code format

--- packages/gogocode-plugin-vue/src/render-function-api.js
+++ packages/gogocode-plugin-vue/src/render-function-api.js
@@ -1,43 +1,41 @@
 const scriptUtils = require('../utils/scriptUtils');
 
 module.exports = function (ast, options) {
-  // 迁移指南: https://v3.cn.vuejs.org/guide/migration/render-function-api.html
-  let scriptAst = ast.parseOptions && ast.parseOptions.language == 'vue' ? ast.find('<script></script>') : ast
+    // 迁移指南: https://v3.cn.vuejs.org/guide/migration/render-function-api.html
+    let scriptAst = ast.parseOptions && ast.parseOptions.language == 'vue' ? ast.find('<script></script>') : ast
 
-  if (scriptAst.length < 1) {
-    return ast
-  }
-  let hName = ''
+    if (scriptAst.length < 1) {
+        return ast
+    }
+    let hName = ''
 
-  scriptAst.find([`render($$$1){$$$2}`, `render: $$$1 => $$$2`, `render: function($$$1){$$$2}`]).each(node => {
-    let params = node.attr('params') || node.attr('value.params') || []
-    params.forEach((item, index) => {
-      hName = item.name
-      params.splice(index, 1);
-      scriptUtils.addVueImport(scriptAst)
-    })
-    node.find(`${hName}($_$,$_$,$_$)`).each(ast => {
-      let args = ast.attr('arguments') || []
-      args.forEach((arg) => {
-        if (arg.type == 'StringLiteral' && arg.value) {
-          ast.replace(`'${arg.value}'`, `Vue.resolveComponent('${arg.value}')`)
+    // 搜集注册组件
+    let components = options && options.components ? options.components : []
+    scriptAst.find(`Vue.component($_$)`).each(c => {
+        let argumentList = c.attr('arguments')
+        if (Array.isArray(argumentList) && argumentList.length > 0) {
+            if (c.attr('arguments')[0].type == 'StringLiteral') {
+                components.push(c.attr('arguments')[0].value)
+                scriptUtils.addVueImport(scriptAst)
+            }
         }
-      })
-      scriptAst.find([`render($_$){}`, `render: $_$ => $_$`]).each(node => {
+    })
+
+    scriptAst.find([`render($$$1){$$$2}`, `render: $$$1 => $$$2`, `render: function($$$1){$$$2}`]).each(node => {
         let params = node.attr('params') || node.attr('value.params') || []
         params.forEach((item, index) => {
-          hName = item.name
-          params.splice(index, 1);
-          scriptUtils.addVueImport(scriptAst)
+            hName = item.name
+            params.splice(index, 1);
+            scriptUtils.addVueImport(scriptAst)
         })
         node.find(`${hName}($_$,$_$,$_$)`).each(ast => {
-          let args = ast.attr('arguments') || []
-          args.forEach((arg, index) => {
-            if (arg.type == 'StringLiteral' && arg.value) {
-              ast.replace(`'${arg.value}'`, `Vue.resolveComponent('${arg.value}')`)
-            }
-            else if (index == 1 && arg.type == 'ObjectExpression') {
-              const plantRenderParaCode = `function plantRenderPara(params) {
+            let args = ast.attr('arguments') || []
+            args.forEach((arg, index) => {
+                if (arg.type == 'StringLiteral' && arg.value) {
+                    ast.replace(`'${arg.value}'`, `Vue.resolveComponent('${arg.value}')`)
+                }
+                else if (index == 1 && arg.type == 'ObjectExpression') {
+                    const plantRenderParaCode = `function plantRenderPara(params) {
             const transProps = {
               staticClass: "class",
               staticStyle: "style",
@@ -81,14 +79,14 @@ module.exports = function (ast, options) {
             }
             return result;
           }`
-              ast.replace(ast.match[0][1].value, `plantRenderPara(${ast.match[0][1].value})`)
-              if (!scriptAst.has(plantRenderParaCode)) {
-                scriptAst.after(plantRenderParaCode)
-              }
-            }
-          })
-          ast.attr('callee.name', 'Vue.h')
+                    ast.replace(ast.match[0][1].value, `plantRenderPara(${ast.match[0][1].value})`)
+                    if (!scriptAst.has(plantRenderParaCode)) {
+                        scriptAst.after(plantRenderParaCode)
+                    }
+                }
+            })
+            ast.attr('callee.name', 'Vue.h')
         })
-      })
-      return ast
-    }
+    })
+    return ast
+}
