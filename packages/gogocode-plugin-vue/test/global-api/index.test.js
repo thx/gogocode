3090062ed95320f9699ea4ccc5ac31fa8ce6acad
付英军 <fuyingjun@alibaba-inc.com>
2021-06-28 21:51:54 +0800

    feat: 处理mount问题

--- packages/gogocode-plugin-vue/test/global-api/index.test.js
+++ packages/gogocode-plugin-vue/test/global-api/index.test.js
@@ -5,13 +5,15 @@ test('global-api: should not throw error', () => {
     expect(() => {
         const jsPath = path.join(__dirname, 'app.js');
         const ast = $.loadFile(jsPath);
-        transform(ast, { gogocode: $ });
+        const options = { rootPath: '/test', filePath: '/test/src/main.js' };
+        transform(ast, { gogocode: $ }, options);
     }).not.toThrow();
 })
 test('global-api: test App', () => {
     const jsPath = path.join(__dirname, 'app.js');
     const ast = $.loadFile(jsPath);
-    const newAst = transform(ast, { gogocode: $ });
+    const options = { rootPath: '/test', filePath: '/test/src/main.js' };
+    const newAst = transform(ast, { gogocode: $ }, options);
     const code = newAst.generate();
     const result = code.indexOf('createApp(App)') > -1;
     expect(result).toBeTruthy();
@@ -22,7 +24,20 @@ test('global-api: test App', () => {
     Vue.use();
     const app = new Vue({ components:{App} })
     `);
-    const newAst = transform(ast, { gogocode: $ });
+    const options = { rootPath: '/test', filePath: '/test/src/main.js' };
+    const newAst = transform(ast, { gogocode: $ }, options);
+    const code = newAst.generate();
+    const result = code.indexOf('createApp(App)') > -1;
+    expect(result).toBeTruthy();
+})
+test('global-api: test mount', () => {
+    const ast = $(`
+    import App from './App';
+    Vue.use();
+    const app = new Vue({ components:{App} }).mount('#app');
+    `);
+    const options = { rootPath: '/test', filePath: '/test/src/main.js' };
+    const newAst = transform(ast, { gogocode: $ }, options);
     const code = newAst.generate();
     const result = code.indexOf('createApp(App)') > -1;
     expect(result).toBeTruthy();
