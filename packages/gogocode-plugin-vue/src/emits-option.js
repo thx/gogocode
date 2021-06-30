1bd1db56063dcf8410914335492bd4f5eb8f4b9b
付英军 <fuyingjun@alibaba-inc.com>
2021-06-30 15:39:22 +0800

    feat: 恢复原来的逻辑

--- packages/gogocode-plugin-vue/src/emits-option.js
+++ packages/gogocode-plugin-vue/src/emits-option.js
@@ -8,8 +8,8 @@ class EmitSet {
     fillEmitSet(ast) {
         ast.find(['$emit($_$,$$$)', '$_$1.$emit($_$,$$$)']).each((fAst) => {
             if (fAst.match && fAst.match[0] && fAst.match[0].length) {
-                let key = fAst.match[0][0].value;
-                if (key === 'input') {
+                let key = fAst.match[0][0].raw;
+                if (key === `'input'` || key === `"input"` || key === '`input`') {
                     //https://v3.cn.vuejs.org/guide/migration/v-model.html#%E8%BF%81%E7%A7%BB%E7%AD%96%E7%95%A5
                     key = `'update:modelValue'`;
                 }
