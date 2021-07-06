8832f01d50e29e30abf6e82c090c17b14fcb5d02
wangmeng <wangmeng01@alibaba-inc.com>
2021-07-06 19:24:53 +0800

    feat: render

--- packages/gogocode-plugin-vue/src/render-function-api.js
+++ packages/gogocode-plugin-vue/src/render-function-api.js
@@ -11,19 +11,26 @@ module.exports = function (ast, api, options) {
     let hName = ''
     scriptAst.find([`render($$$1){$$$2}`, `render: $$$1 => $$$2`, `render: function($$$1){$$$2}`]).each(node => {
         let params = node.attr('params') || node.attr('value.params') || []
-        params.forEach((item, index) => {
-            hName = item.name
-            params.splice(index, 1);
+        if(params.length){
+            hName = params[0].name
+            params.splice(0, 1);
             scriptUtils.addVueImport(scriptAst)
-        })
-        node.find(`${hName}($$$)`).each(ast => {
+        }
+        node.find([`${hName}($$$)`,`${hName}`]).each(ast => {
             let args = ast.attr('arguments') || []
             args.forEach((arg, index) => {
                 if (arg.type == 'StringLiteral' && arg.value && components.indexOf(arg.value) > -1) {
                     ast.replace(`'${arg.value}'`, `Vue.resolveComponent('${arg.value}')`)
                 }
                 else if (index == 1 && arg.type == 'ObjectExpression' && options && options.outRootPath && options.outFilePath) {
-                    const plantRenderParaCode = `export function plantRenderPara(params){const transProps={staticClass:"class",staticStyle:"style",on:"",domProps:"",props:"",attrs:"",};function obj2arr(obj){return typeof obj=="string"?[obj]:Object.keys(obj).map((pk,index)=>{return{[pk]:Object.values(obj)[index]}})}let result={};for(let key in params){if(transProps[key]==null){if(typeof params[key]=="object"){result[key]=obj2arr(params[key])}else{result[key]=params[key]}}}for(let key in params){if(transProps[key]===""){if(typeof params[key]=="object"){Object.assign(result,{...params[key]})}else{result[key]=params[key]}}}for(let key in params){if(transProps[key]){result[transProps[key]]=result[transProps[key]]||[];result[transProps[key]]=result[transProps[key]].concat(obj2arr(params[key]))}}return result}`
+                    const plantRenderParaCode = `export function plantRenderPara(params){const transProps={staticClass:'class',staticStyle:'style',on:'',domProps:'',props:'',attrs:'',}
+                    function obj2arr(obj){return typeof obj=='string'?[obj]:Object.keys(obj).map((pk,index)=>{return{[pk]:Object.values(obj)[index]}})}
+                    let result={}
+                    for(let key in params){if(transProps[key]==null){if(typeof params[key]=='object'){result[key]=obj2arr(params[key])}else{result[key]=params[key]}}}
+                    for(let key in params){if(transProps[key]===''){if(typeof params[key]=='object'){for(let k in params[key]){result[key=='on'?'on'+k.replace(k[0],k[0].toUpperCase()):k]=params[key][k]}}else{result[key]=params[key]}}}
+                    for(let key in params){if(transProps[key]){result[transProps[key]]=result[transProps[key]]||[]
+                      result[transProps[key]]=result[transProps[key]].concat(obj2arr(params[key]))}}
+                    return result}`
                     try {
                         const relativePath = scriptUtils.addUtils(options.outRootPath, options.outFilePath, plantRenderParaCode, api.gogocode)
                         let para = ast.find(`${hName}(${args.map(() => '$_$').join()})`).match[0][1].value
