1ca6c8275b650487090f923eba07bd3a564126a7
bigfengyu <fengyuhere@qq.com>
2021-08-03 21:20:27 +0800

    fix: functional-components

--- packages/gogocode-plugin-vue/src/functional-components.js
+++ packages/gogocode-plugin-vue/src/functional-components.js
@@ -6,34 +6,35 @@ module.exports = function (ast, { gogocode: $ }) {
 
     const isFunctional = script.has('{ functional: true }');
 
-    if (isFunctional) {
-        scriptUtils.addVueImport(script);
-
-        if (isVueFile) {
-            // <template functional> => <template>
-            const tAttr = ast.attr('template.attr');
-            if (tAttr) {
-                delete tAttr.functional;
-            }
+    if (isFunctional && isVueFile) {
+        // <template functional> => <template>
+        const tAttr = ast.attr('template.attr');
+        if (tAttr) {
+            delete tAttr.functional;
         }
-        script.find('{ render: $_$ }').each((ast) => {
+    }
 
-            if(script.has('this.$createElement($$$)')){
-                scriptUtils.addVueImport(script)
-                script.replace('this.$createElement($$$)','Vue.h($$$)')
-            }
+    if (script.find('{ render: $_$ }').length) {
+        scriptUtils.addVueImport(script);
+        if (script.has('this.$createElement($$$)')) {
+            scriptUtils.addVueImport(script);
+            script.replace('this.$createElement($$$)', 'Vue.h($$$)');
+        }
 
-            script.replace('render: ($$$1) => { $$$2 }', 'render($$$1) { $$$2 }')
-            script.replace('render: function ($$$1) { $$$2 }', 'render($$$1) { $$$2 }')
-            let renderFunction = script.find('render() { }')
-            if (renderFunction.length) {
-                const hName = renderFunction.attr('params.0.name');
-                renderFunction.replace(`${hName}`, 'Vue.h');
-                const contextName = renderFunction.attr('params.1.name') || 'context';
+        script.replace('render: ($$$1) => { $$$2 }', 'render($$$1) { $$$2 }');
+        script.replace('render: function ($$$1) { $$$2 }', 'render($$$1) { $$$2 }');
+        let renderFunction = script.find('render() { }');
+        if (renderFunction.length) {
+            const hName = renderFunction.attr('params.0.name');
+            renderFunction.replace(`${hName}`, 'Vue.h');
+            const hasDestructContext = renderFunction.attr('params.1.type') === 'ObjectPattern';
+            const contextName = renderFunction.attr('params.1.name') || 'context';
+            const propsStr = $(renderFunction.attr('params.1')).generate();
 
-                const propsStr = $(renderFunction.attr('params.1')).generate();
-                const hasDestructContext = renderFunction.attr('params.1.type') === 'ObjectPattern';
-                renderFunction.attr('params', []);
+            renderFunction.attr('params', []);
+            
+            if (isFunctional) {
+
                 renderFunction.append('params', '_props');
                 renderFunction.append('params', '_context');
 
@@ -46,9 +47,10 @@ module.exports = function (ast, { gogocode: $ }) {
                     `const ${contextName} = {..._context, props: _props, data: _context.attr, children: _context.slots };`
                 );
                 const renderFunctionStr = `function ${renderFunction.generate()}`;
+
                 renderFunction.parent(1).replaceBy(renderFunctionStr);
             }
-        });
+        }
     }
 
     // remove functional: true
