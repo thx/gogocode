bddc33347226610ddc9d6c56e26fae3a3eb9196d
bigfengyu <fengyuhere@qq.com>
2021-08-03 23:24:27 +0800

    fix: emitterCode

--- packages/gogocode-plugin-vue/src/events-api.js
+++ packages/gogocode-plugin-vue/src/events-api.js
@@ -3,65 +3,64 @@ module.exports = function (ast, api, options) {
     const script = ast.parseOptions && ast.parseOptions.language === 'vue' ? ast.find('<script></script>') : ast;
 
     const emitterCode = `
-    const eventRegistryMap = new WeakMap();
+    const eventRegistryMap = new WeakMap()
     function getRegistry(instance) {
-        let events = eventRegistryMap.get(instance);
-        if (!events) {
-            eventRegistryMap.set(instance, (events = Object.create(null)));
-        }
-        return events;
+      let events = eventRegistryMap.get(instance)
+      if (!events) {
+        eventRegistryMap.set(instance, (events = Object.create(null)))
+      }
+      return events
     }
     export function $on(instance, event, fn) {
-        if (Array.isArray(event)) {
-            event.forEach(e => $on(instance, e, fn));
-        }
-        else {
-            const events = getRegistry(instance);
-            (events[event] || (events[event] = [])).push(fn);
-        }
-        return instance.proxy;
+      if (Array.isArray(event)) {
+        event.forEach((e) => $on(instance, e, fn))
+      } else {
+        const events = getRegistry(instance)
+        ;(events[event] || (events[event] = [])).push(fn)
+      }
+      return instance
     }
     export function $once(instance, event, fn) {
-        const wrapped = (...args) => {
-            off(instance, event, wrapped);
-            fn.call(instance.proxy, ...args);
-        };
-        wrapped.fn = fn;
-        $on(instance, event, wrapped);
-        return instance.proxy;
+      const wrapped = (...args) => {
+        $off(instance, event, wrapped)
+        fn.call(instance, ...args)
+      }
+      wrapped.fn = fn
+      $on(instance, event, wrapped)
+      return instance
     }
     export function $off(instance, event, fn) {
-        const vm = instance.proxy;
-        // all
-        if (!event) {
-            eventRegistryMap.set(instance, Object.create(null));
-            return vm;
-        }
-        // array of events
-        if (Array.isArray(event)) {
-            event.forEach(e => off(instance, e, fn));
-            return vm;
-        }
-        // specific event
-        const events = getRegistry(instance);
-        const cbs = events[event];
-        if (!cbs) {
-            return vm;
-        }
-        if (!fn) {
-            events[event] = undefined;
-            return vm;
-        }
-        events[event] = cbs.filter(cb => !(cb === fn || cb.fn === fn));
-        return vm;
+      const vm = instance
+      // all
+      if (!event) {
+        eventRegistryMap.set(instance, Object.create(null))
+        return vm
+      }
+      // array of events
+      if (Array.isArray(event)) {
+        event.forEach((e) => $off(instance, e, fn))
+        return vm
+      }
+      // specific event
+      const events = getRegistry(instance)
+      const cbs = events[event]
+      if (!cbs) {
+        return vm
+      }
+      if (!fn) {
+        events[event] = undefined
+        return vm
+      }
+      events[event] = cbs.filter((cb) => !(cb === fn || cb.fn === fn))
+      return vm
     }
     export function $emit(instance, event, ...args) {
-        instance.proxy && instance.proxy.$emit && instance.proxy.$emit(event, ...args);
-        const cbs = getRegistry(instance)[event];
-        if (cbs) {
-            cbs.map(cb => cb.apply(instance.proxy, args));
-        }
-        return instance.proxy;
+      instance && instance.$emit && instance.$emit(event, ...args)
+      const cbs = getRegistry(instance)[event]
+      if (cbs) {
+        cbs.map((cb) => cb.apply(instance, args))
+      }
+      return instance
     }
     `;
 
