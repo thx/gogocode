5db01add6f394e076bdda4336594e875de7faf51
wangmeng <wangmeng01@alibaba-inc.com>
2021-10-22 16:28:24 +0800

    fix: vue playground

--- packages/gogocode-vue-playground/packages/vue2/src/components/slots-unification/Comp.vue
+++ packages/gogocode-vue-playground/packages/vue2/src/components/slots-unification/Comp.vue
@@ -2,30 +2,30 @@
   <div>
     <p>迁移：{{ name }}</p>
     <p>Vue版本：{{ version }}</p>
-    <scoped-slots>
+    <ScopedSlots>
       <div slot="header">slot: header</div>
       <div>slot: default</div>
       <div slot="footer">slot: footer</div>
-    </scoped-slots>
+    </ScopedSlots>
   </div>
 </template>
 
 <script>
-	import Vue from 'vue';
+  import { version } from 'vue';
   import ScopedSlots from './ScopedSlots.vue'
   /* 迁移指南: https://v3.cn.vuejs.org/guide/migration/slots-unification.html */
 	export default {
-		name: 'ScopedSlots',
+		name: '插槽统一',
 		props: {
 			msg: String,
 		},
-    components:{
-      ScopedSlots
+    components: {
+      ScopedSlots,
     },
 		data() {
 			return {
 				name: '插槽统一',
-        version: Vue.version
+        version: version
 			};
 		},
 	};
--- packages/gogocode-vue-playground/packages/vue2/src/components/slots-unification/ScopedSlots.vue
+++ packages/gogocode-vue-playground/packages/vue2/src/components/slots-unification/ScopedSlots.vue
@@ -5,20 +5,20 @@
     <slot name="footer"></slot>
   </div>
 </template>
-
 <script>
 export default {
   name: 'ScopedSlots',
-  mounted() {   
-    console.log("vue2", "$slots", this.$slots);   
-    console.log("$scopedSlots.console", this.$scopedSlots.footer);
-    console.log("$scopedSlots.console", this.$scopedSlots.footer());
-
+  mounted() {
     this.$scopedSlots.footer({
       text: "来自子组件的数据",
       name: "aaa",
       id: "aaaaa",
     });
+  },
+  data() {
+    return {
+      slots: this.$scopedSlots
+    };
   },
 };
 </script>
