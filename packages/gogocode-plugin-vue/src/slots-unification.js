6726789b290a0ed9ebe85d1a37d71854d9d0fd8c
chibing.fy <chibing.fy@alibaba-inc.com>
2022-02-22 17:18:17 +0800

    fix: slot-unification bug

--- packages/gogocode-plugin-vue/src/slots-unification.js
+++ packages/gogocode-plugin-vue/src/slots-unification.js
@@ -10,23 +10,27 @@ module.exports = function (ast) {
     })
     let templateAst = ast.find('<template></template>')
     templateAst.find('<$_$ slot="$_$">').each(cast => {        
-        if(cast.attr('content.name') != 'template'){
-            let attrList = cast.attr('content.attributes') || []
-            let valueContent = ''
-            attrList.forEach((attr, index) => {
-                if (attr.value && attr.key && attr.key.content == 'slot') {    
-                    valueContent = attr.value && attr.value.content || ''              
-                    attrList.splice(index, 1);                    
-                }                
-            })
-            if(valueContent){
-                cast.replaceBy(
-                    `<template v-slot:${ valueContent }>
+        // if(cast.attr('content.name') != 'template'){
+        let attrList = cast.attr('content.attributes') || []
+        let valueContent = ''
+        attrList.forEach((attr, index) => {
+            if (attr.value && attr.key && attr.key.content == 'slot') {    
+                valueContent = attr.value && attr.value.content || ''              
+            }                
+        })
+        // 如果直接就是 template 那么修改其属性即可
+        if(valueContent && cast.attr('content.name') === 'template') {
+            const slotAttr = attrList.find(attr => attr.key && attr.key.content == 'slot')
+            delete slotAttr.value
+            slotAttr.key.content = `v-slot:${ valueContent }`
+        } else if(valueContent){
+            cast.replaceBy(
+                `<template v-slot:${ valueContent }>
                     ${ cast.generate() }
                     </template>`
-                );
-            }            
-        }       
+            );
+        }            
+        // }       
     })
     return ast
 }
