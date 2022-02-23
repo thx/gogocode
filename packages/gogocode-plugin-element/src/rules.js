88513e8555d0a309ef843eaf764ce2009ed22408
chibing.fy <chibing.fy@alibaba-inc.com>
2022-02-23 11:00:54 +0800

    chore: 调整规则顺序

--- packages/gogocode-plugin-element/src/rules.js
+++ packages/gogocode-plugin-element/src/rules.js
@@ -4,10 +4,10 @@ const calendar = require('./calendar')
 const dateTimePickers = require('./date-time-pickers')
 const icon = require('./icon')
 const popConfirm = require('./pop-confirm')
-const subMenu = require('./sub-menu')
-const toolTip = require('./tool-tip')
 const popover = require('./popover')
 const scrollbar = require('./scrollbar')
+const subMenu = require('./sub-menu')
+const toolTip = require('./tool-tip')
 
 module.exports = [
     { name: 'import', rule: importRule, test: /\.vue$|\.js$|\.ts$/ },
@@ -16,8 +16,8 @@ module.exports = [
     { name: 'date-time-pickers', rule: dateTimePickers, test: /\.vue$|\.js$|\.ts$/ },
     { name: 'icon', rule: icon, test: /\.vue$/ },
     { name: 'pop-confirm', rule: popConfirm, test: /\.vue$/ },
-    { name: 'sub-menu', rule: subMenu, test: /\.vue$/ },
-    { name: 'tool-tip', rule: toolTip, test: /\.vue$/ },
     { name: 'popover', rule: popover, test: /\.vue$/ },
     { name: 'scrollbar', rule: scrollbar, test: /\.vue$/ },
+    { name: 'sub-menu', rule: subMenu, test: /\.vue$/ },
+    { name: 'tool-tip', rule: toolTip, test: /\.vue$/ },
 ];
