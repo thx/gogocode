130a95358f89d844d23a59263be80cccc7893d4e
chibing.fy <chibing.fy@alibaba-inc.com>
2022-02-23 23:26:07 +0800

    fix: .native 可以不出现在最后一位，限定是 v-on or @

--- packages/gogocode-vue-playground/packages/vue3/src/components/v-on-native-modifier-removed/Comp-out.vue
+++ packages/gogocode-vue-playground/packages/vue3/src/components/v-on-native-modifier-removed/Comp-out.vue
@@ -3,14 +3,14 @@
     <p>迁移：{{ name }}</p>
     <p>Vue版本：{{ version }}</p>
     <InputComp v-on:change="handleChange" /><br />
-    <InputComp v-on:focus.native="handleFocus" />
-    <InputComp @focus.native.prevent="handleFocus" />
+    <InputComp v-on:focus="handleFocus" />
+    <InputComp @focus.prevent="handleFocus" />
   </div>
 </template>
 
 <script>
-import Vue from 'vue';
-import InputComp from './InputComp';
+import * as Vue from 'vue'
+import InputComp from './InputComp'
 /* 迁移指南: https://v3.cn.vuejs.org/guide/migration/v-on-native-modifier-removed.html */
 export default {
   name: '移除 v-on.native 修饰符',
@@ -21,23 +21,22 @@ export default {
     return {
       name: '移除 v-on.native 修饰符',
       version: Vue.version,
-    };
+    }
   },
   methods: {
     handleChange: () => {
-      console.log('change');
+      console.log('change')
     },
     handleFocus: () => {
-      console.log('focus');
+      console.log('focus')
     },
   },
   components: {
     InputComp,
   },
-};
+}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1 {
   color: #64b587;
