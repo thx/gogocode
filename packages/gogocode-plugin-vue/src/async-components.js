cfac0814819e7ac0ca963754611be0584f45b022
chibing.fy <chibing.fy@alibaba-inc.com>
2022-02-23 23:29:47 +0800

    fix: async-components test when add vue import

--- packages/gogocode-plugin-vue/src/async-components.js
+++ packages/gogocode-plugin-vue/src/async-components.js
@@ -1,24 +1,29 @@
 const scriptUtils = require('../utils/scriptUtils');
-module.exports = function(ast) {
-    const script =
-    ast.parseOptions && ast.parseOptions.language === 'vue'
-        ? ast.find('<script></script>')
-        : ast;
-    scriptUtils.addVueImport(script);
-    script
-        .replace('() => import($_$)', 'Vue.defineAsyncComponent(() => import($_$))')
-        .replace(
-            `
+module.exports = function (ast) {
+    const script = ast.parseOptions && ast.parseOptions.language === 'vue' ? ast.find('<script></script>') : ast;
+    if (
+        ast.has([
+            '() => import($_$)',
+            `() => ({
+              component: import($_$1),
+              $$$
+            })`,
+        ])
+    ) {
+        scriptUtils.addVueImport(script);
+    }
+    script.replace('() => import($_$)', 'Vue.defineAsyncComponent(() => import($_$))').replace(
+        `
         () => ({
           component: import($_$1),
           $$$
         })`,
-            `
+        `
       Vue.defineAsyncComponent({
         loader: () => import($_$1),
         $$$
       })
         `
-        );
+    );
     return ast;
 };
