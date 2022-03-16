4f66f585a7d939a0260615041703531face76ee6
chibing.fy <chibing.fy@alibaba-inc.com>
2022-03-16 19:35:53 +0800

    chore: build with polyfills

--- packages/gogocode-core/rollup.config.js
+++ packages/gogocode-core/rollup.config.js
@@ -1,26 +1,15 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import builtins from 'rollup-plugin-node-builtins';
-import replace from '@rollup/plugin-replace';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 import { terser } from 'rollup-plugin-terser';
-import insert from 'rollup-plugin-insert';
 import pkg from './package.json';
 
 const commonPlugins = [
     json(),
-    builtins(),
-    resolve(), // so Rollup can find `ms`
-    commonjs(), // so Rollup can convert `ms` to an ES module
-    replace({
-        'process.env.NODE_ENV': JSON.stringify('production'),
-        'process.platform': JSON.stringify('darwin'),
-        'process.env': '{}',
-        'process.stdout': 'null',
-    }),
-    insert.prepend(
-        `const global = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};`
-    ),
+    commonjs(),
+    resolve({ preferBuiltins: false, browser: true }),
+    nodePolyfills(),
 ];
 
 export default [
@@ -32,6 +21,12 @@ export default [
             file: pkg.browser,
             format: 'umd',
         },
+        onwarn: function (warning) {
+            if (warning.code === 'THIS_IS_UNDEFINED') {
+                return;
+            }
+            console.error(warning.message);
+        },
         plugins: commonPlugins,
     },
     {
@@ -41,24 +36,12 @@ export default [
             file: 'umd/gogocode.min.js',
             format: 'umd',
         },
+        onwarn: function (warning) {
+            if (warning.code === 'THIS_IS_UNDEFINED') {
+                return;
+            }
+            console.error(warning.message);
+        },
         plugins: [...commonPlugins, terser()],
     },
-
-    // CommonJS (for Node) and ES module (for bundlers) build.
-    // (We could have three entries in the configuration array
-    // instead of two, but it's quicker to generate multiple
-    // builds from a single configuration where possible, using
-    // an array for the `output` option, where we can specify
-    // `file` and `format` for each target)
-    // {
-    //     input: 'index.js',
-    //     output: [
-    //         { file: pkg.main, format: 'cjs' },
-    //         { file: pkg.module, format: 'es' },
-    //     ],
-    //     plugins: [
-    //         resolve(), // so Rollup can find `ms`
-    //         commonjs(), // so Rollup can convert `ms` to an ES module
-    //     ],
-    // },
 ];
