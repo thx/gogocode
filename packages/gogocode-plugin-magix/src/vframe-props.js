eec7068041a5da95a083e147f9478ada9a0913ba
chibing.fy <chibing.fy@alibaba-inc.com>
2022-06-16 09:33:29 +0800

    fix: vframe props 转换

--- packages/gogocode-plugin-magix/src/vframe-props.js
+++ packages/gogocode-plugin-magix/src/vframe-props.js
@@ -1,14 +1,24 @@
 module.exports = function ({ script, template }) {
-    if(!template) { return { script, template }; }
-    template.find(['<mx-vframe></mx-vframe>', '<mx-vframe />']).each((res) => {
-        res.each((frameElement) => {
-            const attrs = frameElement.attr('content.attributes') || [];
-            attrs.forEach((attr) => {
-                const keyName = attr?.key?.content || '';
-                if (keyName && keyName !== 'src' && !keyName.startsWith('*')) {
-                    attr.key.content = '*' + keyName;
-                }
-            });
+    if (!template) {
+        return { script, template };
+    }
+    template.find(['<$_$></$_$>', '<$_$ />']).each((res) => {
+        res.each((tag) => {
+            const tagName = tag.match?.[0]?.[0]?.value || '';
+            if (tagName.startsWith('mx-')) {
+                const attrs = tag.attr('content.attributes') || [];
+                attrs.forEach((attr) => {
+                    const keyName = attr?.key?.content || '';
+                    if (
+                        keyName &&
+                        ['src', 'class', 'style', 'id'].includes(keyName) &&
+                        !keyName.startsWith('*') &&
+                        !keyName.startsWith('mx-')
+                    ) {
+                        attr.key.content = '*' + keyName;
+                    }
+                });
+            }
         });
     });
     return { script, template };
